adde5bde281acbc458f051d926ad003bddf64814
KashafSyeda <50900045+KashafSyeda@users.noreply.github.com>
2019-06-15 17:29:55 +0500

    Add files via upload

---
+++ star_pyramid.py
@@ -0,0 +1,14 @@
+''' OUTPUT
+
+  *
+ ***
+*****
+'''
+
+num = int(input("enter number of rows"))
+for i in range(0, num):
+    for j in range(0, num-i-1):
+        print(end=" ")  # space are created 
+    for k in range(0, i+1):
+        print("*", end=" ")     
+    print()           
